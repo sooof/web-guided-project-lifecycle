dceecb653c0cd68353eaa5f1261066d73da64865
sooof <sooof@me.com>
2021-10-26 19:41:25 -0700

    Fetch dogs

--- doggos/src/App.js
+++ doggos/src/App.js
@@ -3,23 +3,86 @@ import axios from 'axios';
 
 class App extends React.Component {
     state = {
-        dogImages: [
-        "https://images.dog.ceo/breeds/hound-afghan/n02088094_1003.jpg",
-        "https://images.dog.ceo/breeds/hound-afghan/n02088094_1007.jpg",
-        "https://images.dog.ceo/breeds/hound-afghan/n02088094_1023.jpg",
-        "https://images.dog.ceo/breeds/hound-afghan/n02088094_10263.jpg",
-        "https://images.dog.ceo/breeds/hound-afghan/n02088094_10715.jpg",
-        "https://images.dog.ceo/breeds/hound-walker/n02089867_1988.jpg"
-        ]
+        dogImages: [],
+        input: ""
+    }
+    componentDidMount() {
+        axios.get('https://dog.ceo/api/breed/husky/images')
+            .then( resp=> {
+                // console.log(resp.data);
+                //console.log(resp.data.message  );
+                this.setState({
+                    ...this.state,
+                    dogImages: resp.data.message
+                });
+            }).catch( err=>{
+                console.log(err);
+            });
+    }
+
+    handleChange = (e) => {
+        //console.log("handleChangle" );
+        this.setState({
+            ...this.state,
+            input: e.target.value
+        });
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        //1. Did we update dogImages?
+        //2. Is that update tied to Chihuahuas?
+        //3. If so, make a new axios call on huskies?
+        
+        console.log("State/Props Update");
+
+        // if (prevState.dogImages !== this.state.dogImages) {
+        //     console.log("DogImages Update");
+        //     if (this.state.input === "chihuahua") {
+        //         axios.get('https://dog.ceo/api/breed/husky/images')
+        //             .then(resp=> {
+        //                 this.setState({
+        //                     ...this.state,
+        //                     dogImages: resp.data.message
+        //                 });
+        //             })
+        //             .catch(err=> {
+        //                 console.log(err);
+        //             })
+        //     }
+        // }
+
+        if (this.state.input === "chihuahua") {
+            this.setState({
+                ...this.state,
+                input: "husky"
+            })
+        }
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        //hound  pug
+        axios.get(`https://dog.ceo/api/breed/${this.state.input}/images`)
+        .then(resp=> {
+            console.log(resp);
+            this.setState({
+                ...this.state,
+                dogImages: resp.data.message
+            })
+        })
+        .catch(err=> {
+            console.log(err);
+        })
     }
 
     render (){
+        console.log(this.state.input);
         return(
             <div>
                 <h1>Dog Pic Finder 3.2</h1>
-                <form>
-                    <input/>
-                    <button>Fetch Dogs</button>
+                <form >
+                    <input onChange={this.handleChange}/>
+                    <button onClick={this.handleSubmit}>Fetch Dogs</button>
                 </form>
                 <div>
                     {
@@ -27,7 +90,6 @@ class App extends React.Component {
                             <img src={dogImage} key={dogImage.id}/>
                         ))
                     }
-                    {/* <img src="https://images.dog.ceo/breeds/hound-afghan/n02088094_1003.jpg"/> */}
                 </div>
             </div>
         )
