5a08824f36bd34004ede7c756c3201c93d5e2008
sooof <sooof@me.com>
2021-11-30 10:22:27 -0800

    doggos: start

--- doggos/src/App.js
+++ doggos/src/App.js
@@ -1,2 +1,18 @@
 import React from 'react';
 import axios from 'axios';
+
+class App extends React.Component {
+    render() {
+        return(<div>
+            <h1>Dog Image Retriever v1.4</h1>
+            <form>
+                <input/>
+                <button>Search</button>
+            </form>
+            <div id="dogImages">
+                <img src=""/>
+            </div>
+        </div>);
+    }
+}
+export default App;
