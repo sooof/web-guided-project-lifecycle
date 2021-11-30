1a2b5e11a8806f765d352947f583b01119789af2
sooof <sooof@me.com>
2021-11-30 10:28:13 -0800

    doggos: use componentDidMount mount data

--- doggos/src/App.js
+++ doggos/src/App.js
@@ -2,18 +2,25 @@ import React from 'react';
 import axios from 'axios';
 
 class App extends React.Component {
+    // state = {
+    //     dogImages: [
+    //         "https://images.dog.ceo/breeds/hound-afghan/n02088094_1003.jpg",
+    //         "https://images.dog.ceo/breeds/hound-afghan/n02088094_1007.jpg",
+    //         "https://images.dog.ceo/breeds/hound-afghan/n02088094_1023.jpg",
+    //         "https://images.dog.ceo/breeds/hound-afghan/n02088094_10263.jpg"
+    //     ]
+    // }
     state = {
-        dogImages: [
-            "https://images.dog.ceo/breeds/hound-afghan/n02088094_1003.jpg",
-            "https://images.dog.ceo/breeds/hound-afghan/n02088094_1007.jpg",
-            "https://images.dog.ceo/breeds/hound-afghan/n02088094_1023.jpg",
-            "https://images.dog.ceo/breeds/hound-afghan/n02088094_10263.jpg"
-        ]
+        dogImages: []
     }
+
     componentDidMount() {
         axios.get('https://dog.ceo/api/breed/hound/images')
             .then(resp=> {
-                console.log(resp);
+                this.setState({
+                    ...this.state,
+                    dogImages:resp.data.message
+                });
             })
             .catch(err=> {
                 console.log(err);
