1b0e4d2a97f64e2d0774d8186cc81c7f8ecc3c08
sooof <sooof@me.com>
2021-12-01 14:44:45 -0800

    show friends

--- review/src/index.js
+++ review/src/index.js
@@ -2,12 +2,20 @@
    
 import React, { Component } from "react";
 import { render } from 'react-dom'
+import "./styles.scss"
 
 import {people} from './people';
 
 console.log("people:  === ", people)
 
 class App extends Component {
+    constructor() {
+        console.log("constructor Invoked!" )
+        super();
+        this.state = {
+          friends: people
+        }
+      }
 
 
   render() {
@@ -20,6 +28,9 @@ class App extends Component {
           more 'at bats' with ReactJS and to talk about the pieces of the{" "}
           <strong>React Component LifeCycle</strong> along the way.
         </p>
+        {this.state.friends.map(friend => ( 
+        <div key={friend.id}> {friend.first_name} </div>
+        ))}
 
       </div>
     );
