8e91d66c9276e3c1841424d2b633d9e4e39cc6cc
sooof <sooof@me.com>
2021-12-01 14:23:43 -0800

    use useLocalStorage

--- review/src/index.js
+++ review/src/index.js
@@ -1,8 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles.scss';
-import AppFunc from './AppFunc';
-import AppClass from './AppClass';
 
-const rootElement = document.getElementById('root');
-ReactDOM.render(<AppFunc />, rootElement);
+   
+import React, { Component } from "react";
+import { render } from 'react-dom'
+
+
+class App extends Component {
+
+
+  render() {
+    console.log("Render Invoked ")
+    return (
+      <div className="App">
+        <h1> Welcome to Friends! </h1>
+        <p>
+          The purpose of this follow along assignment is to give you access to
+          more 'at bats' with ReactJS and to talk about the pieces of the{" "}
+          <strong>React Component LifeCycle</strong> along the way.
+        </p>
+
+      </div>
+    );
+  }
+}
+
+
+
+
+const rootElement = document.getElementById("root");
+
+render(
+  //
+  // React.createElement(App), 
+  <App />,
+  rootElement
+  );
