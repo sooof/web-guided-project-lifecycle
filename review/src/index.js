b4814ce74625f38171bdb6b0fae5e026921e29c2
sooof <sooof@me.com>
2021-12-01 16:40:11 -0800

    componentDidMount

--- review/src/index.js
+++ review/src/index.js
@@ -1,8 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles.scss';
-import AppFunc from './AppFunc';
-import AppClass from './AppClass';
+import React from "react";
+import ReactDOM from "react-dom";
 
-const rootElement = document.getElementById('root');
-ReactDOM.render(<AppFunc />, rootElement);
+import "./styles.scss";
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      doggos: []
+    };
+  }
+  componentDidMount() {
+    fetch("https://dog.ceo/api/breed/labrador/images")
+      .then((res) => res.json())
+      .then((dogs) => this.setState({ doggos: dogs.message }))
+      .catch((err) => console.log("noooo"));
+  }
+  render() {
+    console.log(this.state);
+    return (
+      <div className="App">
+        <h1>Hello CodeSandbox</h1>
+        <h2>Start editing to see some magic happen!</h2>
+        {this.state.doggos.map((doggo) => (
+          <img src={doggo} key={doggo} />
+        ))}
+      </div>
+    );
+  }
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
