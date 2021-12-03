88c51b62b08513612020a217d0188d21128ed5ed
sooof <sooof@me.com>
2021-12-03 15:41:57 -0800

    create getDogImage

--- doggos/src/App.js
+++ doggos/src/App.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import axios from 'axios';
 
+const getDogImages = (breed) => {
+   const promise =  axios.get(`https://dog.ceo/api/breed/${breed}/images`)
+
+    return promise
+}
+
 class App extends React.Component {
-    // state = {
-    //     dogImages: [
-    //         "https://images.dog.ceo/breeds/hound-afghan/n02088094_1003.jpg",
-    //         "https://images.dog.ceo/breeds/hound-afghan/n02088094_1007.jpg",
-    //         "https://images.dog.ceo/breeds/hound-afghan/n02088094_1023.jpg",
-    //         "https://images.dog.ceo/breeds/hound-afghan/n02088094_10263.jpg"
-    //     ]
-    // }
     state = {
         dogImages: [],
-        breed: ""
+        breed: "pug"
     }
 
+
     componentDidMount(prevProps, prevState) {
-        axios.get('https://dog.ceo/api/breed/hound/images')
+        getDogImages(this.state.breed)
+        // axios.get(`https://dog.ceo/api/breed/${this.state.breed}/images`)
             .then(resp=> {
                 this.setState({
                     ...this.state,
@@ -28,25 +28,15 @@ class App extends React.Component {
             })
     }
     componentDidUpdate(prevProps, prevState) {
-        console.log("Props are updating")
+        // console.log("Props are updating")
         // console.log('old state: ', prevState);
-        // if(prevState.dogImages !== this.state.dogImages){
-        //     console.log("Changes to dogImages")
-        //     if(this.state.breed === "chihuahua"){
-        //         axios.get('https://dog.ceo/api/breed/husky/images')
-        //         .then(resp=> {
-        //             this.setState({
-        //                 ...this.state,
-        //                 dogImages:resp.data.message,
-        //                 breed: "husky"
-        //             });
-        //         })
-        //         .catch(err=> {
-        //             console.log(err);
-        //         });
-            
-        //     }
-        // }
+        if(prevState.dogImages !== this.state.dogImages){
+            console.log("componentDidUpdate: Changes to dogImages this.state.breed=", this.state.breed)
+            if(this.state.breed === "chihuahua"){
+                console.log("In side")
+                getDogImages("husky")
+            }
+        }
         console.log("changes state ", this.state);
         // if (this.state.breed === "chihuahua") {
         //     this.setState({
@@ -62,18 +52,7 @@ class App extends React.Component {
         // console.log(this.state.breed);
         //2. capture submit
         //3. execute axios call based on user input
-        axios.get(`https://dog.ceo/api/breed/${this.state.breed}/images`)
-            .then(resp=> {
-                // console.log(resp);
-                this.setState({
-                    ...this.state,
-                    dogImages: resp.data.message
-                })
-            })
-            .catch(err=> {
-                console.log(err);
-            })
-        //4. change this.state.dogImages to response
+        // this.getDogImages(this.state.breed)
     }
     handleChange = (e) => {
         // axios.get(`https://dog.ceo/api/breed/${this.state.breed}/images`)
