0866eb37695cf691a18f986c9e98340300e93889
sooof <sooof@me.com>
2021-12-03 16:12:21 -0800

    mkdir servies && use  .then  work all

--- doggos/src/App.js
+++ doggos/src/App.js
@@ -1,28 +1,29 @@
 import React from 'react';
 import axios from 'axios';
+import { getDogImages } from './services/getDogImages';
 
-const getDogImages = (breed) => {
-   return axios.get(`https://dog.ceo/api/breed/${breed}/images`)
-   .then(resp=> {
-    // this.setState({
-    //     ...this.state,
-    //     dogImages:resp.data.message
-    // });
-    console.log("frist 1", resp)
-    return (resp.data)
-    })
-    .then(data=>{
-        console.log("second 2", data)
-        return (data.message)
-    })
-    .then(images => {
-        console.log("last 1", images)
-        return (images)
-    })
-    .catch(err=> {
-        console.log(err);
-    })
-}
+// const getDogImages = (breed) => {
+//    return axios.get(`https://dog.ceo/api/breed/${breed}/images`)
+//    .then(resp=> {
+//     // this.setState({
+//     //     ...this.state,
+//     //     dogImages:resp.data.message
+//     // });
+//     console.log("frist 1", resp)
+//     return (resp.data)
+//     })
+//     .then(data=>{
+//         console.log("second 2", data)
+//         return (data.message)
+//     })
+//     .then(images => {
+//         console.log("last 1", images)
+//         return (images)
+//     })
+//     .catch(err=> {
+//         console.log(err);
+//     })
+// }
 
 class App extends React.Component {
     state = {
